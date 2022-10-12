80cd26e7883c38bfdf31a779d5282ce4b8ff7182
William Gadd <wgadd@sasktel.net>
2022-10-12 23:04:26 +0000

    Revmoved console.log and moved submit functionto client.js

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,18 +1,15 @@
 $(document).ready(function() {
-  const textBox = $('section.new-tweet > form > textarea');
+  const form = $('section.new-tweet').children('form');
+  const textBox = form.children('textarea');
   textBox.on('input', function (event) {
     const { value, textLength } = this;
     const counter = $(this).parent().find('.counter').get(0); 
     const newCount = 140 - textLength;
     counter.innerHTML = newCount;
     assignLengthClass(counter);
-    console.log(newCount);
   });
 
 });
-
-// Test input
-// This is a great commenting system. What do you think? Will it pass the great counter number test? Let us see in a few seconds. How
 
 const assignLengthClass = function(counter) {
   if( parseInt(counter.innerHTML) < 0) {
