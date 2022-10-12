e1f543a46e4cde27cb059336eb0ce7d25675ed60
William Gadd <wgadd@sasktel.net>
2022-10-12 23:49:24 +0000

    Refactered character counting script by adding a helper function checking if the message is too long

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -6,16 +6,20 @@ $(document).ready(function() {
     const counter = $(this).parent().find('.counter').get(0); 
     const newCount = 140 - textLength;
     counter.innerHTML = newCount;
-    assignLengthClass(counter);
+    assignLengthClass(counter, isTooLong);
   });
 
 });
 
-const assignLengthClass = function(counter) {
-  if( parseInt(counter.innerHTML) < 0) {
-    $(counter).addClass('invalid-length');
-    return;
-  }
+const isTooLong = (count => count < 0);
+
+const assignLengthClass = function(counter, callback) {
+  // if( parseInt(counter.innerHTML) < 0) {
+    const count = parseInt(counter.innerHTML);
+    if (callback(count)) {
+      $(counter).addClass('invalid-length');
+      return;
+    }
 
   $(counter).removeClass('invalid-length');
 };
