b21eb3f521c599625f1d3bf939f23f22592b631a
William Gadd <wgadd@sasktel.net>
2022-10-14 01:49:33 +0000

    Use eslint to find and fix linting errors

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,49 +6,49 @@
 
 $('document').ready(function() {
       
-      $('#form-error').hide()
-      
-      const createTweetElement = function(tweetObject) {
-        const {user, content, created_at: dateCreated} = tweetObject;
-        
-        const $tweet = $('<article class="tweet">');
-        
-        const $header = $('<header>');
-        
-        
-        const $user = $('<span class="user">');
-        
-        console.log('user', user);
-        
-        const $userImage = $('<img>')
-        .attr('src', user.avatars);
-        
-        
-        const $userName = $(`<span>`)
-        .text(user.name);
-        
-        $user.append($userImage)
-        .append($userName);
-        
-        
-        const $handle = $('<span class="account-name">')
-        .text(user.handle);
-        
-        
-        $header.append([$user, $handle]);
-        
-        const $body = $(`<span class="content">`)
-        .text(content.text);
+  $('#form-error').hide();
+  
+  const createTweetElement = function(tweetObject) {
+    const {user, content, created_at: dateCreated} = tweetObject;
+    
+    const $tweet = $('<article class="tweet">');
+    
+    const $header = $('<header>');
+    
+    
+    const $user = $('<span class="user">');
+    
+    console.log('user', user);
+    
+    const $userImage = $('<img>')
+      .attr('src', user.avatars);
+    
+    
+    const $userName = $(`<span>`)
+      .text(user.name);
+    
+    $user.append($userImage)
+      .append($userName);
+    
+    
+    const $handle = $('<span class="account-name">')
+      .text(user.handle);
+    
+    
+    $header.append([$user, $handle]);
+    
+    const $body = $(`<span class="content">`)
+      .text(content.text);
 
 
     const $footer = $('<footer>');
 
-   const timePosted = timeago.format(dateCreated);
+    const timePosted = timeago.format(dateCreated);
 
     const $postTime = $('<span class="post-time">')
       .text(timePosted);
 
-    const $reactions=$(`
+    const $reactions = $(`
     <span class="reactions">
       <i class="fa-solid fa-flag react>
       <i class="fa-solid fa-flag react"></i>
@@ -81,48 +81,48 @@ $('document').ready(function() {
     const counter = $(this).children('.footer').children('.counter');
     console.log(counter);
     $('#form-error').slideUp(() => {
-      try{
-        postText(this, inputBox, counter); 
+      try {
+        postText(this, inputBox, counter);
       } catch (error) {
         renderError(error.message);
       }
     });
   });
 
-  const renderError = function (error) {
+  const renderError = function(error) {
     $('#form-error').text(error);
-      $('#form-error').slideDown();
-      return;
+    $('#form-error').slideDown();
+    return;
   };
 
   const postText = function(form, inputBox, counter) {
-    const { textLength } = inputBox
+    const { textLength } = inputBox;
 
     if (textLength === 0) throw new Error("There is no message to post");
     if (textLength > 140) throw new Error("Content is too long");
 
-    const query = $( form ).serialize();
+    const query = $(form).serialize();
     
     $.ajax('/tweets/', { method: 'POST', data: query})
-      .then(function (data, error) {
+      .then(function() {
         inputBox.value = "";
         counter.text("140");
         return $.ajax('/tweets/', { method: 'GET'});
       })
-      .then (function (tweets) {
+      .then(function(tweets) {
         renderTweets(tweets.slice(-1));
         
       });
 
-  }
+  };
 
   const loadTweets = function() {
     $.ajax('/tweets/', { method: 'GET'})
-    .then(tweets => {
-      renderTweets(tweets);
-    })
-    .catch(error => console.log(error));
-  }
+      .then(tweets => {
+        renderTweets(tweets);
+      })
+      .catch(error => console.log(error));
+  };
 
   loadTweets();
 
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   const form = $('section.new-tweet').children('form');
   const textBox = form.children('textarea');
-  textBox.on('input', function (event) {
-    const { value, textLength } = this;
-    const counter = $(this).parent().find('.counter').get(0); 
+  textBox.on('input', function() {
+    const { textLength } = this;
+    const counter = $(this).parent().find('.counter').get(0);
     const newCount = 140 - textLength;
     counter.innerHTML = newCount;
     assignLengthClass(counter, isTooLong);
@@ -14,12 +14,11 @@ $(document).ready(function() {
 const isTooLong = (count => count < 0);
 
 const assignLengthClass = function(counter, callback) {
-  // if( parseInt(counter.innerHTML) < 0) {
-    const count = parseInt(counter.innerHTML);
-    if (callback(count)) {
-      $(counter).addClass('invalid-length');
-      return;
-    }
+  const count = parseInt(counter.innerHTML);
+  if (callback(count)) {
+    $(counter).addClass('invalid-length');
+    return;
+  }
 
   $(counter).removeClass('invalid-length');
 };
